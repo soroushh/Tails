abc96f2278484e6ac6dfa9fa8c14de873e0c765d
student <sorush.kh68@gmail.com>
2019-06-06 10:34:12 +0100

    We can calculate the total VAT in pennies.

--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -24,10 +24,15 @@ describe Order do
   end
 
   context "#total_VAT" do
+
     it "We can calcualte the total VAT of an order\
     with accuracy of 0.01 and an exchange rate different from 1." do
       ex_rate = 2.5
       expect(@order.total_VAT(0,ex_rate)).to eq (2*599*0.2 + 3*250*0)*2.5.round(2)
+    end
+
+    it "We can calculate the total VAT of an order in pennies with accuracy of 1" do
+      expect(@order.penny_total_VAT()).to eq((2*599*0.2 + 3*250*0).round())
     end
   end
 
